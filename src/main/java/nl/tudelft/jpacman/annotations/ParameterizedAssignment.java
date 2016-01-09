c1206257e97c2f5d6458a910fae40107935f2b50
LiamClark <L.A.Clark@student.tudelft.nl>
2016-01-09 21:41:39 +0100

    added the ParameterizedAssignment annotation

---
+++ src/main/java/nl/tudelft/jpacman/annotations/ParameterizedAssignment.java
@@ -0,0 +1,8 @@
+package nl.tudelft;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface ParameterizedAssignment {
+}
