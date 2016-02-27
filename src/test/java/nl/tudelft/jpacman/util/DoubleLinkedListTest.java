3f92df14187fbcf663b1b9a26d4c4afa96f02f4c
Angeall <angeal1105@gmail.com>
2016-02-27 22:43:05 +0100

    Integration problem with the TestSuite of DoubleLinkedListTest Here's a try to fix it : Changed the JUnit runner

--- src/test/java/nl/tudelft/jpacman/util/DoubleLinkedListTest.java
+++ src/test/java/nl/tudelft/jpacman/util/DoubleLinkedListTest.java
@@ -3,46 +3,41 @@ package nl.tudelft.jpacman.util;
 import com.google.common.collect.testing.*;
 import com.google.common.collect.testing.features.CollectionFeature;
 import com.google.common.collect.testing.features.CollectionSize;
-import com.google.common.collect.testing.features.ListFeature;
 import junit.framework.TestSuite;
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
 import org.junit.runners.Suite;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Queue;
 
 /**
  * Your test class must be annotated with {@link RunWith} to specify that it's a
  * test suite and not a single test.
  */
-@RunWith(Suite.class)
+@RunWith(value=AllTests.class)
 /**
  * We need to use static inner classes as JUnit only allows for empty "holder"
  * suite classes.
  */
-@Suite.SuiteClasses({
-        DoubleLinkedListTest.GuavaTests.class,
-        //DoubleLinkedListTest.AdditionalTests.class,
-})
+@Suite.SuiteClasses(value={DoubleLinkedListTest.class})
 
 
 /**
  * Created by Angeall on 27/02/2016.
  */
 public class DoubleLinkedListTest {
-    public static class GuavaTests {
-
-        /**
-         * The method responsible for returning the {@link TestSuite} generated
-         * by one of the {@link FeatureSpecificTestSuiteBuilder} classes.
-         * This method must be public static method with no arguments named
-         * "suite()".
-         *
-         * @return An instance of {@link TestSuite} for collection testing.
-         */
+//    public static class GuavaTests {
+//
+//        /**
+//         * The method responsible for returning the {@link TestSuite} generated
+//         * by one of the {@link FeatureSpecificTestSuiteBuilder} classes.
+//         * This method must be public static method with no arguments named
+//         * "suite()".
+//         *
+//         * @return An instance of {@link TestSuite} for collection testing.
+//         */
         public static TestSuite suite() {
-            /**
+            /**&
              * guava-testlib has a host of test suite builders available,
              * all descending from {@link FeatureSpecificTestSuiteBuilder}.
              * The
@@ -69,7 +64,7 @@ public class DoubleLinkedListTest {
                     // You can optionally give a name to your test suite. This
                     // name is used by JUnit and other tools during report
                     // generation.
-                    .named("ArrayList tests")
+                    .named("DoubleLinkedList tests")
                     // Guava has a host of "features" in the
                     // com.google.common.collect.testing.features package. Use
                     // them to specify how the collection should behave, and
@@ -85,6 +80,6 @@ public class DoubleLinkedListTest {
         }
 
 
-    }
+//    }
 
 }
