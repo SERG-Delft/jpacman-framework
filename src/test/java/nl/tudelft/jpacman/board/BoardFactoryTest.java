675e6ad1c9a295bd4762b5bdac4bbe07ae778ef6
Angeall <angeal1105@gmail.com>
2016-03-02 23:03:50 +0100

    Added the tests for the untested methods in BoardFactory

--- src/test/java/nl/tudelft/jpacman/board/BoardFactoryTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardFactoryTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.jpacman.board;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import nl.tudelft.jpacman.sprite.PacManSprites;
 
@@ -92,5 +94,57 @@ public class BoardFactoryTest {
 		factory.createBoard(grid);
 		assertEquals(s2, s1.getSquareAt(Direction.SOUTH));
 		assertEquals(s1, s2.getSquareAt(Direction.SOUTH));
+	}
+
+    /**
+     * Verifies that the
+     * @throws Exception
+     */
+	@Test
+	public void testCreateBoard() throws Exception {
+		Square s1 = new BasicSquare();
+		Square s2 = new BasicSquare();
+		Square s3 = new BasicSquare();
+		Square s4 = new BasicSquare();
+		Square s5 = new BasicSquare();
+		Square s6 = new BasicSquare();
+		Square[][] grid = new Square[][]{{s1, s2}, {s3, s4}, {s5, s6}};
+		Board board = factory.createBoard(grid);
+        assertEquals(s1, board.squareAt(0, 0));
+        assertEquals(s2, board.squareAt(0, 1));
+        assertEquals(s3, board.squareAt(1, 0));
+        assertEquals(s4, board.squareAt(1, 1));
+        assertEquals(s5, board.squareAt(2, 0));
+        assertEquals(s6, board.squareAt(2, 1));
+	}
+
+	@Test
+	public void testCreateInfiniteBoard() throws Exception {
+		Square s1 = new BasicSquare();
+		Square s2 = new BasicSquare();
+		Square s3 = new BasicSquare();
+		Square s4 = new BasicSquare();
+		Square s5 = new BasicSquare();
+		Square s6 = new BasicSquare();
+		Square[][] grid = new Square[][]{{s1, s2}, {s3, s4}, {s5, s6}};
+		InfiniteBoard board = factory.createInfiniteBoard(grid);
+		assertEquals(s1, board.squareAt(0, 0));
+		assertEquals(s2, board.squareAt(0, 1));
+		assertEquals(s3, board.squareAt(1, 0));
+		assertEquals(s4, board.squareAt(1, 1));
+		assertEquals(s5, board.squareAt(2, 0));
+		assertEquals(s6, board.squareAt(2, 1));
+	}
+
+	@Test
+	public void testCreateGround() throws Exception {
+		Square ground = factory.createGround();
+		assertTrue(ground.isAccessibleTo(new BasicUnit()));
+	}
+
+	@Test
+	public void testCreateWall() throws Exception {
+		Square ground = factory.createWall();
+		assertFalse(ground.isAccessibleTo(new BasicUnit()));
 	}
 }
