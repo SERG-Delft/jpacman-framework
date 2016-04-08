ef56042ec49e9869b1f5ec71ab34d8a480921ff8
corentin <bserax@gmail.com>
2016-04-08 12:48:20 +0200

    ajout de la classe superPellet

---
+++ src/main/java/nl/tudelft/jpacman/level/SuperPelLet.java
@@ -0,0 +1,18 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.sprite.Sprite;
+
+/**
+ * Created by Corentin on 08-04-16.
+ */
+public class SuperPellet extends Pellet {
+    /**
+     * Creates a new pellet.
+     *
+     * @param points The point value of this pellet.
+     * @param sprite The sprite of this pellet.
+     */
+    public SuperPellet(int points, Sprite sprite) {
+        super(points, sprite);
+    }
+}
