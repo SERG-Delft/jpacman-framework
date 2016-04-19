bfa3b069c6fa5e219f4338fa74ffce9889577164
LiamClark <L.A.Clark@student.tudelft.nl>
2016-04-19 14:32:48 +0200

    added an example for th estudents in the javadoc of the ParameterizedAssignment for grading

--- src/main/java/nl/tudelft/jpacman/annotations/ParameterizedAssignment.java
+++ src/main/java/nl/tudelft/jpacman/annotations/ParameterizedAssignment.java
@@ -3,6 +3,17 @@ package nl.tudelft.jpacman.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * A marker annotation for the auto-grader to identify a boundary testing solution.
+ *
+ * A Students Solution class for the boundary testing exercise should have the following:
+ *
+ * @ParameterizedAssignment
+ * @RunWith(Parameterized.class)
+ * public class WithinBordersTest {
+ *     //tests
+ * }
+ */
 @Retention(RetentionPolicy.CLASS)
 public @interface ParameterizedAssignment {
 }
