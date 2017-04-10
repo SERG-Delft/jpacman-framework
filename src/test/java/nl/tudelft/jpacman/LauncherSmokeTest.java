f7aaf4cf4fe174f633c67f252307cff25a2380b7
Arie van Deursen <arie.vandeursen@tudelft.nl>
2017-04-10 22:23:05 +0200

    Disable checks for JUnit test cases.
    Initialization done in setup methods, not covered by
    checker framework.
    
    Some null results are known not to occur in the test case,
    so warnings can be ignored.

--- src/test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
+++ src/test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
  *
  * @author Arie van Deursen, March 2014.
  */
+@SuppressWarnings("initialization")
 public class LauncherSmokeTest {
 	
 	private Launcher launcher;
--- src/test/java/nl/tudelft/jpacman/board/BasicSquare.java
+++ src/test/java/nl/tudelft/jpacman/board/BasicSquare.java
@@ -22,6 +22,7 @@ class BasicSquare extends Square {
 	}
 
 	@Override
+	@SuppressWarnings("return.type.incompatible")
 	public Sprite getSprite() {
 		return null;
 	}
